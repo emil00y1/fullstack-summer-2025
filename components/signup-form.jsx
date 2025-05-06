affa4b687df2aaf6c06ab461d2500f35ff5e1702
Emil Abrahamson <emilabrahamson2@gmail.com>
2025-05-06 10:23:39 +0200

    Add SignUpForm component and integrate Radix UI Label #1

---
+++ components/signup-form.jsx
@@ -0,0 +1,78 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function SignUpForm({
+  className,
+  ...props
+}) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Sign Up</CardTitle>
+          <CardDescription>
+            Enter your email below to sign up your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+                <Label htmlFor="name">Display Name</Label>
+                <Input
+                  id="display-name"
+                  type="name"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="name">Username</Label>
+                <Input
+                  id="user-name"
+                  type="name"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="email@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Signup
+              </Button>
+              <Button variant="outline" className="w-full">
+                Sign up with Google
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Login
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
