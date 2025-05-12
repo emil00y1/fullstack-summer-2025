83db37b0425ba243fa1530f78dbcf2176e5b993f
Emil Kristensen <emil00y1@stud.kea.dk>
2025-05-12 10:56:58 +0200

    initial gitlab pipeline

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,13 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 300,
-      aggregateTimeout: 150,
-    };
-    return config;
-  },
+  // webpackDevMiddleware: (config) => {
+  //   config.watchOptions = {
+  //     poll: 300,
+  //     aggregateTimeout: 150,
+  //   };
+  //   return config;
+  // },
 };
 
 // Change this line - use ES Module export syntax instead of CommonJS
